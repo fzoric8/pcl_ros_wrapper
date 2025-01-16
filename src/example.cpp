7528bf7fef9c2e16fbf03d96fc0d2367b13a4d31
Filip Zoric <filip.zoric@fer.hr>
2025-01-16 16:39:02 +0100

    Added init voxel filtering for the RGBD camera

--- src/example.cpp
+++ src/example.cpp
@@ -4,19 +4,32 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
 ros::Publisher pub;
 
 void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-  // Create a container for the data.
+    // Container for original & filtered data
+  pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+  pcl::PCLPointCloud2 cloud_filtered;
+
+  // Convert to PCL data type
+  pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+  // Perform the actual filtering
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  sor.setInputCloud (cloudPtr);
+  sor.setLeafSize (0.1, 0.1, 0.1);
+  sor.filter (cloud_filtered);
+
+  // Convert to ROS data type
   sensor_msgs::PointCloud2 output;
+  pcl_conversions::fromPCL(cloud_filtered, output);
 
-  // Do data processing here...
-  output = *input;
-
-  // Publish the data.
+  // Publish the data
   pub.publish (output);
 }
 
@@ -28,10 +41,10 @@ main (int argc, char** argv)
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
+  ros::Subscriber sub = nh.subscribe ("/camera/depth/color/points", 1, cloud_cb);
 
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+  pub = nh.advertise<sensor_msgs::PointCloud2> ("/filtered_pcl", 1);
 
   // Spin
   ros::spin ();
